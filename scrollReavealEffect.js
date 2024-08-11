5245441f4e491ad66d8bebc32e725871aa2279b3
AblixM <arehammasresha890@gmail.com>
2024-08-11 20:38:51 +0300

    chore: Update scroll reveal effects and dependencies

---
+++ scrollReavealEffect.js
@@ -0,0 +1,26 @@
+// ScrollReveal().reveal(".nav-bar", { delay: 200 });
+// ScrollReveal().reveal(".hero-container", { delay: 200 });
+ScrollReveal().reveal(".member-number", {
+  delay: 400,
+  easing: "ease-out",
+  interval: 200,
+  scale: 1.2,
+  reset: true,
+  desktop: true,
+  mobile: false,
+});
+
+ScrollReveal().reveal(".overview", { delay: 200, scale: 1.2, reset: true });
+
+ScrollReveal().reveal(".stats", {
+  delay: 200,
+  easing: "ease-out",
+  interval: 200,
+  scale: 1.2,
+  reset: true,
+  desktop: true,
+  mobile: false,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".button", { delay: 500, origin: "bottom" });
