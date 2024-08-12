85db35fc11745e889f72958e4a774e284c489157
AblixM <arehammasresha890@gmail.com>
2024-08-12 18:37:24 +0300

    ..

--- scrollReavealEffect.js
+++ scrollReavealEffect.js
@@ -1,28 +1,31 @@
 ScrollReveal().reveal(".nav-bar", { delay: 200 });
 ScrollReveal().reveal(".hero-container", { delay: 200 });
-ScrollReveal().reveal(".member-number", {
-  delay: 400,
-  easing: "ease-out",
-  interval: 200,
-  scale: 1.2,
-  reset: true,
-  desktop: true,
-  mobile: false,
-});
-
+ScrollReveal().reveal(".member-number", { delay: 200, reset: true });
 ScrollReveal().reveal(".overview", { delay: 200, reset: true });
+ScrollReveal().reveal(".stats", { delay: 200 });
+ScrollReveal().reveal(".hero-container", { delay: 200 });
 
-ScrollReveal().reveal(".stats", {
-  delay: 200,
-  easing: "ease-out",
-  interval: 200,
-  reset: true,
-  desktop: true,
-  mobile: false,
-  origin: "left",
-});
+// ScrollReveal().reveal(".member-number", {
+//   delay: 400,
+//   easing: "ease-out",
+//   interval: 200,
+//   scale: 1.2,
+//   reset: true,
+//   desktop: true,
+//   mobile: false,
+// });
 
-ScrollReveal().reveal(".button", { delay: 500, origin: "bottom" });
+// ScrollReveal().reveal(".overview", { delay: 200, reset: true });
+
+// ScrollReveal().reveal(".stats", {
+//   delay: 200,
+//   easing: "ease-out",
+//   interval: 200,
+//   reset: true,
+//   desktop: true,
+//   mobile: false,
+//   origin: "left",
+// });
 
 ScrollReveal().reveal(".hero-text-project", { delay: 400, origin: "left" });
 ScrollReveal().reveal(".project-card", {
