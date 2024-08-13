e8fa484a3efad8892c5e5d28a8a8a6c164e3c922
Hheran <heranhabtamu@gmail.com>
2024-08-13 13:36:43 +0300

    Updated form.js

--- form.js
+++ form.js
@@ -44,6 +44,42 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
       document.getElementById("popupOverlay").style.display = "block";
       document.getElementById("popupWindow").style.display = "block";
       document.getElementById("myForm").reset();
+      const duration = 15 * 1000,
+        animationEnd = Date.now() + duration;
+
+      let skew = 1;
+
+      function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+      }
+
+      (function frame() {
+        const timeLeft = animationEnd - Date.now(),
+          ticks = Math.max(200, 500 * (timeLeft / duration));
+
+        skew = Math.max(0.8, skew - 0.001);
+
+        confetti({
+          particleCount: 1,
+          startVelocity: 0,
+          ticks: ticks,
+          origin: {
+            x: Math.random(),
+            // since particles fall down, skew start toward the top
+            y: Math.random() * skew - 0.2,
+          },
+          colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+          shapes: ["circle"],
+          gravity: randomInRange(0.4, 0.6),
+          scalar: randomInRange(0.4, 1),
+          drift: randomInRange(-0.4, 0.4),
+        });
+
+        if (timeLeft > 0) {
+          requestAnimationFrame(frame);
+        }
+      })();
+
     }, 1000);
   }
 
