269e081bae27a53d4591c73d9ff3c578a9ebbbc4
ablixM <abrehammasresha890@gmail.com>
2024-08-21 21:07:12 +0300

    feat: Update Lenis configuration for smoother scrolling experience

--- lenis.js
+++ lenis.js
@@ -1,4 +1,7 @@
-const lenis = new Lenis();
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+});
 
 lenis.on("scroll", (e) => {
   console.log(e);
@@ -6,6 +9,7 @@ lenis.on("scroll", (e) => {
 
 function raf(time) {
   lenis.raf(time);
+  // ScrollTrigger.update();
   requestAnimationFrame(raf);
 }
 
